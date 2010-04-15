565321f5a7103e061ff135911598ec13f8ea9ab4
hawk.ac@gmx.net <hawk.ac@gmx.net@2760c078-044b-11df-9726-c7cc4aaf1e85>
2010-04-15 11:26:05 +0000

    Added Mux dll

---
+++ trunk/BluRip/Encode.cs
@@ -0,0 +1,227 @@
+﻿//BluRip - one click BluRay/m2ts to mkv converter
+//Copyright (C) 2009-2010 _hawk_
+
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either version 2
+//of the License, or (at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+//Contact: hawk.ac@gmx.net
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.IO;
+using MediaInfoLib;
+
+namespace BluRip
+{
+    public partial class MainForm : Form
+    {
+        private EncodeTool et = null;
+
+        private void buttonDoEncode_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!checkX264()) return;
+                progressBarMain.Visible = true;
+                buttonAbort.Visible = true;
+                tabControlMain.Enabled = false;
+
+                DoEncode();
+            }
+            catch (Exception ex)
+            {
+                MessageEncode("Exception: " + ex.Message);
+            }
+            finally
+            {
+                tabControlMain.Enabled = true;
+                progressBarMain.Visible = false;
+                buttonAbort.Visible = false;
+            }
+        }
+
+        private void EncodeMsg(object sender, ExternalTool.MsgEventArgs e)
+        {
+            MessageEncode(e.Message.Replace("\b", "").Trim());
+        }
+
+        private bool DoEncode()
+        {
+            try
+            {
+                this.Text = title + " [Encoding...]";
+                notifyIconMain.Text = this.Text;
+
+                if (settings.workingDir == "")
+                {
+                    MessageMain("Working dir not set");
+                    if (!silent) MessageBox.Show("Working dir not set", "Error");
+                    return false;
+                }
+                if (demuxedStreamList.streams.Count == 0)
+                {
+                    MessageMain("No demuxed streams available");
+                    if (!silent) MessageBox.Show("No demuxed streams available", "Error");
+                    return false;
+                }
+
+                VideoFileInfo vfi = null;
+
+                foreach (StreamInfo si in demuxedStreamList.streams)
+                {
+                    if (si.streamType == StreamType.Video)
+                    {
+                        if (si.extraFileInfo.GetType() == typeof(VideoFileInfo))
+                        {
+                            vfi = (VideoFileInfo)si.extraFileInfo;
+                            break;
+                        }
+                    }
+                }
+
+                if (vfi == null || vfi.encodeAvs == "")
+                {
+                    MessageMain("Encode avs not set - do index + autocrop first");
+                    if (!silent) MessageBox.Show("Encode avs not set - do index + autocrop first", "Error");
+                    return false;
+                }
+
+                int profile = comboBoxEncodeProfile.SelectedIndex;
+                if (profile < 0)
+                {
+                    MessageMain("Encoding profile not set");
+                    if (!silent) MessageBox.Show("Encoding profile not set", "Error");
+                    return false;
+                }
+
+                et = new EncodeTool(settings, profile, false, vfi);
+                et.OnInfoMsg += new ExternalTool.InfoEventHandler(EncodeMsg);
+                et.OnLogMsg += new ExternalTool.LogEventHandler(EncodeMsg);
+                et.Start();
+                et.WaitForExit();                
+
+                if (!et.Successfull) return false;
+                if (settings.encodingSettings[profile].pass2)
+                {
+                    et = new EncodeTool(settings, profile, true, vfi);
+                    et.OnInfoMsg += new ExternalTool.InfoEventHandler(EncodeMsg);
+                    et.OnLogMsg += new ExternalTool.LogEventHandler(EncodeMsg);
+                    et.Start();
+                    et.WaitForExit();
+                    return et.Successfull;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageEncode("Exception: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                this.Text = title;
+                notifyIconMain.Text = this.Text;
+            }
+        }
+
+        private class OutputReader
+        {
+
+            private StreamReader sr = null;
+            private string text = "";
+            public string Text { get { return text; } }
+            private MsgHandler Message = null;
+            private string lastMsg = "";
+            private UserSettings settings = null;
+            private int index = -1;
+            private MainForm mf = null;
+            private NotifyIcon ni = null;
+
+            public OutputReader(StreamReader sr, MsgHandler Message, UserSettings settings, int index, MainForm mf, NotifyIcon ni)
+            {
+                try
+                {
+                    this.sr = sr;
+                    this.Message = Message;
+                    this.settings = settings;
+                    this.index = index;
+                    this.mf = mf;
+                    this.ni = ni;
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+            public void Start()
+            {
+                try
+                {
+                    while ((text = sr.ReadLine()) != null)
+                    {
+                        int s = text.IndexOf('[');
+                        int e = text.IndexOf(']');
+                        if (s == 0 && e > 0)
+                        {
+                            string substr = text.Substring(s + 1, e - s - 2);
+                            if (substr != lastMsg)
+                            {
+                                Message(text);
+                                lastMsg = substr;
+
+                                if (index > -1 && settings.encodingSettings[index].pass2)
+                                {
+                                    if (!mf.secondPass)
+                                    {
+                                        mf.Text = mf.title + " [Encoding 1. pass " + text + "]";
+                                        ni.Text = "Encoding 1. pass - " + substr + "%";
+                                    }
+                                    else
+                                    {
+                                        mf.Text = mf.title + " [Encoding 2. pass " + text + "]";
+                                        ni.Text = "Encoding 2. pass - " + substr + "%";
+                                    }
+                                }
+                                else
+                                {
+                                    mf.Text = mf.title + " [Encoding " + text + "]";
+                                    ni.Text = "Encoding - " + substr + "%";
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Message(text);
+                        }
+                    }
+                    text = sr.ReadToEnd();
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        public bool secondPass = false;
+    }
+}
