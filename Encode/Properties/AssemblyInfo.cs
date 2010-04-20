fe9f823ef429439de8f83126e58b3f54393ccc2e
hawk.ac@gmx.net <hawk.ac@gmx.net@2760c078-044b-11df-9726-c7cc4aaf1e85>
2010-04-20 14:06:41 +0000

    Fixed copy&paste bug in mux dll

--- Encode/Properties/AssemblyInfo.cs
+++ Encode/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.3761.1")]
+[assembly: AssemblyVersion("0.4.3762.2")]
