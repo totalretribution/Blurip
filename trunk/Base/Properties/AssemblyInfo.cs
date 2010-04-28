511fd5edd727edffe144ead4b1a4e30df6dbaf0e
hawk.ac@gmx.net <hawk.ac@gmx.net@2760c078-044b-11df-9726-c7cc4aaf1e85>
2010-04-28 10:53:58 +0000

    Save complete log when using 'Start' button even when an error occurs

--- trunk/Base/Properties/AssemblyInfo.cs
+++ trunk/Base/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.3762.3")]
+[assembly: AssemblyVersion("0.4.3770.4")]
