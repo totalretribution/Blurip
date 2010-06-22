09bc48853244aaaf2d56dffdbf9abf709f059188
hawk.ac@gmx.net <hawk.ac@gmx.net@2760c078-044b-11df-9726-c7cc4aaf1e85>
2010-06-22 11:53:07 +0000

    Added first wpf projects

---
+++ trunk/BluRipWpf/LogWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BluRip
+{
+    /// <summary>
+    /// Interaktionslogik für LogWindow.xaml
+    /// </summary>
+    public partial class LogWindow : Window
+    {
+        private delegate void Message(string msg);
+
+        public LogWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                e.Cancel = true;
+                this.Hide();
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        public void MessageMain(string msg)
+        {
+            try
+            {
+                if (richTextBoxMainLog.Dispatcher.CheckAccess())
+                {                    
+                    richTextBoxMainLog.AppendText(msg + "\r");
+                    richTextBoxMainLog.ScrollToEnd();
+                }
+                else
+                {
+                    richTextBoxMainLog.Dispatcher.Invoke(new Message(MessageMain), new object[] { msg });
+                }                
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void MessageDemux(string msg)
+        {
+            try
+            {
+                if (richTextBoxDemuxLog.Dispatcher.CheckAccess())
+                {
+                    richTextBoxDemuxLog.AppendText(msg + "\r");
+                    richTextBoxDemuxLog.ScrollToEnd();
+                    MessageMain(msg);
+                }
+                else
+                {
+                    richTextBoxDemuxLog.Dispatcher.Invoke(new Message(MessageDemux), new object[] { msg });
+                }
+                
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ trunk/BluRipWpf/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BluRip
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UserSettings settings = new UserSettings();
+        private string settingsPath = "";
+
+        private List<string> videoTypes = new List<string>();
+        private List<string> ac3AudioTypes = new List<string>();
+        private List<string> dtsAudioTypes = new List<string>();
+
+        public string title = "BluRip v0.5.0 © _hawk_";
+
+        private LogWindow logWindow = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            try
+            {
+                videoTypes.Add("h264/AVC");
+                videoTypes.Add("VC-1");
+                videoTypes.Add("MPEG2");
+
+                ac3AudioTypes.Add("TrueHD/AC3");
+                ac3AudioTypes.Add("AC3");
+                ac3AudioTypes.Add("AC3 Surround");
+                ac3AudioTypes.Add("AC3 EX");
+                ac3AudioTypes.Add("E-AC3");
+                ac3AudioTypes.Add("RAW/PCM"); // convert to ac3 by default
+
+                dtsAudioTypes.Add("DTS");
+                dtsAudioTypes.Add("DTS Master Audio");
+                dtsAudioTypes.Add("DTS Express");
+                dtsAudioTypes.Add("DTS Hi-Res");
+                dtsAudioTypes.Add("DTS ES"); // have to check if needed
+                dtsAudioTypes.Add("DTS-ES");
+
+                /*
+                comboBoxX264Priority.Items.Clear();
+                foreach (string s in Enum.GetNames(typeof(ProcessPriorityClass)))
+                {
+                    comboBoxX264Priority.Items.Add(s);
+                }
+                */
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //CheckForIllegalCrossThreadCalls = false;
+                this.Title = title;
+                settingsPath = AppDomain.CurrentDomain.BaseDirectory + "\\settings.xml";
+
+                if (!File.Exists(settingsPath))
+                {
+                    UserSettings.SaveSettingsFile(settings, settingsPath);
+                    UserSettings.LoadSettingsFile(ref settings, settingsPath);
+                }
+                else
+                {
+                    UserSettings.LoadSettingsFile(ref settings, settingsPath);
+                }
+                //UpdateFromSettings();
+                logWindow = new LogWindow();
+                logWindow.Owner = this;
+
+                UpdateStatus((string)App.Current.Resources["StatusBar"] + " " + (string)App.Current.Resources["StatusBarReady"]);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void menuItemViewLog_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (menuItemViewLog.IsChecked)
+                {
+                    logWindow.Show();
+                }
+                else
+                {
+                    logWindow.Hide();
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void buttonBluRayPath_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                FolderBrowserDialog fbd = new FolderBrowserDialog();
+                if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    textBoxBlurayPath.Text = fbd.SelectedPath;
+                    settings.lastBluRayPath = fbd.SelectedPath;
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void UpdateStatus(string msg)
+        {
+            try
+            {
+                statusBarItemMain.Content = msg;
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
