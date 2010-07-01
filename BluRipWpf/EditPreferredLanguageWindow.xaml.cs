f8ffce51140e8836cbe34e923457bf31f0ca2e0e
hawk.ac@gmx.net <hawk.ac@gmx.net@2760c078-044b-11df-9726-c7cc4aaf1e85>
2010-07-01 14:14:48 +0000

    Wpf: added all settings + preferred subtitle language

---
+++ BluRipWpf/EditPreferredLanguageWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BluRip
+{
+    /// <summary>
+    /// Interaktionslogik für EditPreferedLanguageWindow.xaml
+    /// </summary>
+    public partial class EditPreferredLanguageWindow : Window
+    {
+        private LanguageInfo li = null;
+
+        public EditPreferredLanguageWindow(LanguageInfo li)
+        {
+            InitializeComponent();
+            try
+            {
+                this.li = new LanguageInfo(li);
+
+                textBoxLanguage.Text = li.language;
+                textBoxTranslation.Text = li.translation;
+                textBoxLanguageShort.Text = li.languageShort;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public LanguageInfo languageInfo
+        {
+            get { return li; }
+        }
+
+        private void buttonOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void textBoxLanguage_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                li.language = textBoxLanguage.Text;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void textBoxTranslation_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                li.translation = textBoxTranslation.Text;
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        private void textBoxLanguageShort_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                li.languageShort = textBoxLanguageShort.Text;
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
